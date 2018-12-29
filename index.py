182e430dfd774d16f75e118e482511747271b5a9
v0ltis <40314287+v0ltis@users.noreply.github.com>
2018-12-29 10:35:24 +0100

    Create index.py

---
+++ index.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+
+client = discord.Client()
+chat_filter = ["PUTE","SALOPE","CONNARD","CUL","ABRUTIT","NIQUE","ENCULE","CHATTE","BITE","CON"]
+bypass_list = ["362615539773997056","437289213616979968"]
+
+@client.event
+async def on_ready():
+    print("Logged in as:", client.user.name)
+    print("ID:", client.user.id)
+
+@client.event
+async def on_message(message):    
+    if message.content.upper().startswith("~PING"):
+      timePing = time.monotonic()
+      pinger = await client.send_message(message.channel, ":ping_pong: **Pong !**")
+      ping = "%.2f"%(1000* (time.monotonic() - timePing))
+      await client.edit_message(pinger, ":ping_pong: **Pong !**\n\
+`Ping:" + ping + "`")
+    if message.content.upper().startswith("~SAY"):
+          args = message.content.split(" ")
+          await client.send_message(message.channel, (" ".join(args[1:])))
+    contents = message.content.split(" ")
+    for word in contents:
+        if word.upper() in chat_filter:
+           if not message.author.id in bypass_list:
+                await client.delete_message(message)
+                await client.send_message(message.channel, "**Hey!** un peut de respect!!!")
+            
+client.run(process.env.TOKEN)
+
+
