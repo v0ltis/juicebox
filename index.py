cbecac934886b6df3ad1d8d7e7252e48f012cdbc
v0ltis <40314287+v0ltis@users.noreply.github.com>
2018-12-30 09:03:13 +0100

    Update index.py

--- index.py
+++ index.py
@@ -10,6 +10,7 @@ bypass_list = ["362615539773997056","437289213616979968"]
 
 @client.event
 async def on_ready():
+    await client.charge_presence(game-object.Game(name="~help"))
     print("Logged in as:", client.user.name)
     print("ID:", client.user.id)
 
