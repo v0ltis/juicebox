e9237ee71cb2d48f777976d6333071dd80ecfaf0
v0ltis <40314287+v0ltis@users.noreply.github.com>
2018-12-30 10:27:54 +0100

    Update index.py

--- index.py
+++ index.py
@@ -34,14 +34,13 @@ async def on_message(message):
                 await client.delete_message(message)
                 await client.send_message(message.channel, "**Hey!** un peut de respect!!!")
    
- async def displayembed():
     if message.content.upper().startswith("~HELP"):
-      help = discord.Embed(colour=discord.Colour(0x6f0053), description="Commandes :\n**~Help:**  affiche les commandes\n**~Say** (+text): dit le text\n**~Ping** : affiche le ping\n- - - - - - - - - - - - - - - - - - -\nL**iens** :\n[Chaine YouTube de Silvathor](https://www.youtube.com/channel/UCe_nGFDs5_r1hRbL5l3JcfQ)\n[Inviter des personnes sur notre serveur discord](https://discord.me/https://discord.me/minecraft_tips)")
+      embed = discord.Embed(colour=discord.Colour(0x6f0053), description="Commandes :\n**~Help:**  affiche les commandes\n**~Say** (+text): dit le text\n**~Ping** : affiche le ping\n- - - - - - - - - - - - - - - - - - -\nL**iens** :\n[Chaine YouTube de Silvathor](https://www.youtube.com/channel/UCe_nGFDs5_r1hRbL5l3JcfQ)\n[Inviter des personnes sur notre serveur discord](https://discord.me/https://discord.me/minecraft_tips)")
 
       embed.set_author(name="Juice Box", icon_url="https://discordemoji.com/assets/emoji/JuiceBox.png")
       embed.set_footer(text="https://discord.me/minecraft_tips")
     
-      await client.say(embed=help)
+      await client.say(embed=embed)
              
 
             
