f57ceb16b9a366dd161734aaffcf306635ab89f0
v0ltis <40314287+v0ltis@users.noreply.github.com>
2018-12-31 08:55:51 +0100

    Update index.py

--- index.py
+++ index.py
@@ -8,42 +8,35 @@ import os
 client = discord.Client()
 chat_filter = ["PUTE","SALOPE","CONNARD","CUL","ABRUTIT","NIQUE","ENCULE","CHATTE","BITE","CON","BITCH","PUTIN","FOUTRE","ASS","TRISO","GOGOL","COQUIN","BATARDE","FELATION","SEX"]
 bypass_list = ["362615539773997056","437289213616979968"]
-bot = commands.Bot(command_prefix='~',description='Description')
-@client.event
-async def on_ready():
-    await client.change_presence(game=discord.Game(name="~help"))
-    print("Logged in as:", client.user.name)
-    print("ID:", client.user.id)
 
 @client.event
-async def on_message(message):    
-    if message.content.upper().startswith("PING"):
-      timePing = time.monotonic()
-      pinger = await client.send_message(message.channel, ":ping_pong: **Pong !**")
-      ping = "%.2f"%(1000* (time.monotonic() - timePing))
-      await client.edit_message(pinger, ":ping_pong: **Pong !**\n\
-`Ping:" + ping + "`")
-        
-    if message.content.upper().startswith("SAY"):
-          args = message.content.split(" ")
-          await client.send_message(message.channel, (" ".join(args[1:])))
-          await bot.delete_message(ctx.message)
-    contents = message.content.split(" ")
-    for word in contents:
-           if word.upper() in chat_filter:
-             if not message.author.id in bypass_list:
-                await client.delete_message(message)
-                await client.send_message(message.channel, "**Hey!** un peut de respect!!!")
-   
-    @bot.command(pass_context=True)
-    async def embed(ctx):
-         embed = discord.Embed(name="help", color=0x542765)
-         embed.add_field(name="Juice Boxe", value=ctx.message.author.name)
-         embed.set_Footer(text="TEST".format(ctx.message.author.name))
-         await bot.say(embed=embed)
-            
-client.run(os.environ['TOKEN_BOT'])
-
-
-
+ async def on_ready():
+     await client.change_presence(game=discord.Game(name="~help"))
+     print("Logged in as:", client.user.name)
+     print("ID:", client.user.id)
+ 
+ @client.event
+ async def on_message(message):    
+     if message.content.upper().startswith("~PING"):
+       timePing = time.monotonic()
+       pinger = await client.send_message(message.channel, ":ping_pong: **Pong !**")
+       ping = "%.2f"%(1000* (time.monotonic() - timePing))
+       await client.edit_message(pinger, ":ping_pong: **Pong !**\n\
+ `Ping:" + ping + "`")
+         
+     if message.content.upper().startswith("~SAY"):
+           args = message.content.split(" ")
+           await client.send_message(message.channel, (" ".join(args[1:])))
+     contents = message.content.split(" ")
+     for word in contents:
+            if word.upper() in chat_filter:
+              if not message.author.id in bypass_list:
+                 await client.delete_message(message)
+                 await client.send_message(message.channel, "**Hey!** un peut de respect!!!")
+    
+     if message.content.upper().startswith("~HELP"):
+       await client.send_message(message.channel, "__**Préfix :**__ ~ \n __**Commandes:**__ \n __~help__ : Affiche les commandes \n __~say__*(+text)*: dit le text \n__~ping__: affiche le ping") 
+ 
+             
+ client.run(os.environ['TOKEN_BOT'])
 
