ad1fbda653d6e09fd7a59451758811eb783e13b8
v0ltis <40314287+v0ltis@users.noreply.github.com>
2019-01-09 16:22:26 +0100

    Update index.py

--- index.py
+++ index.py
@@ -42,7 +42,7 @@ async def on_message(message):
         help.add_field(name="/help", value="Affiche les commandes", inline=True)
         help.add_field(name="/say (+texte)", value="Fait dire au bot le texte", inline=True)
         help.add_field(name="/ping", value="Affiche le ping", inline=True)
-     await client.send_message(message.channel, embed=help)
+        await client.send_message(message.channel, embed=help)
     
     
     
