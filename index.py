9a79f8ca5522acb091b202e66d7d72d27b57f53e
v0ltis <40314287+v0ltis@users.noreply.github.com>
2019-01-09 17:47:54 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext.commands import Bot
-from discord.ext.commands import has_permissions, MissingPermissions
+from discord.ext.commands import has_permissions
+from discord.ext.commands import MissingPermissions
 from discord.ext import commands
 import asyncio
 import time
