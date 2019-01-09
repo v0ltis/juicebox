e43cafdc5524a28586c49e39a34da36840ec2774
v0ltis <40314287+v0ltis@users.noreply.github.com>
2019-01-09 17:01:25 +0100

    Update index.py

--- index.py
+++ index.py
@@ -64,8 +64,10 @@ async def on_message(message):
     if message.content.upper().startswith("GG"):
        await client.send_message(message.channel,":clap: :clap: :clap:") 
 
+    if message.content.upper().startswith("/botAdmin")
+        await client.delete_message(message)
+        await client.send_message(message.channel,"Hey Boss , code here: https://github.com/v0ltis/juicebox/edit/master/index.py")
         
-   
 
 
     
