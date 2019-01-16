4fd3ea1d8e355dead75e88b7ba28b3a035adc1cc
v0ltis <40314287+v0ltis@users.noreply.github.com>
2019-01-16 14:10:09 +0100

    Update index.py

--- index.py
+++ index.py
@@ -134,7 +134,7 @@ async def leave(ctx):
 	except:
 		print("Error ...")
 		message_channel = ctx.message.channel
-		message_content = ("Error ...Please wait and try again.Or try '//join'.")
+		message_content = "Error ...Please wait and try again.Or try '//join'."
                 await client.send_message(message_channel,message_content)
 
 
