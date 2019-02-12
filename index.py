16d31f742f33e3f4685625b044e3f3edee2452d6
v0ltis <40314287+v0ltis@users.noreply.github.com>
2019-02-12 12:02:06 +0100

    Update index.py

--- index.py
+++ index.py
@@ -60,7 +60,6 @@ async def on_message(message):
         args = message.content.split(" ")
         await client.send_message(message.channel, "Votre ticket a bien été envoyé au staff , merci !")
         await client.send_message(discord.Object(id="544830498099298324"), "Ticket de:" + message.author + "\n dans le serveur " + guild.name +"avec l'ID " + guild.id + "\n Pour la raiosn suivante :" + args[1:])
-      contents = message.content.split(" ")
       
     
     for word in contents:
