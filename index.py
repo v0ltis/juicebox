25d4923d37462c9fd09544bbe1632304b1e12cf4
v0ltis <40314287+v0ltis@users.noreply.github.com>
2019-02-14 10:59:20 +0100

    Update index.py

--- index.py
+++ index.py
@@ -65,7 +65,6 @@ async def on_message(message):
       
     if message.content.upper().startswith("/TICKET"):
         args = message.content.split(" ")
-        await client.send_message(message.channel, "Votre ticket a bien été envoyé au staff , merci !")
         ticket = ["Ticket de l'utilisateur: ", message.author, " Avec l'ID: ``", message.author.id, "``\nPour la raison suivante: "]
         separator = " "
         ticket_message = separator.join(ticket)
