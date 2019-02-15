a04b6e23ba560ca2975554e55968f96d63be3eba
v0ltis <40314287+v0ltis@users.noreply.github.com>
2019-02-15 12:57:48 +0100

    Update index.py

--- index.py
+++ index.py
@@ -199,7 +199,7 @@ async def on_message(message):
         await client.send_message(message.channel,"Hey Boss , code here: https://github.com/v0ltis/juicebox/edit/master/index.py")
     
     if message.content.upper().startswith("/INFO"):
-      await client.send_message(message.channel, message.mentions.name)
+      await client.send_message(message.channel, message.mentions.user)
 
     #join
     if message.content.upper().startswith("/JOIN"):
