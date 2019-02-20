c5ff56f537640b560d6b6016b19df76edaf2a997
v0ltis <40314287+v0ltis@users.noreply.github.com>
2019-02-20 12:04:34 +0100

    Update index.py

--- index.py
+++ index.py
@@ -33,8 +33,8 @@ async def on_ready():
 		await client.change_presence(game=discord.Game(name='/help'))
 		await client.send_message(discord.Object(id='543490625773895681'), 'Redemarage effectué !')
 		await client.send_message(discord.Object(id='543490625773895681'), "Les serveurs auquel je suis connecté sont :\n")
-	       	for x in client.servers:
-      			serveur_juicy = await client.send_message(discord.Object(id='547731369988587530'), x.name)
+		for x in client.servers:
+			serveur_juicy = await client.send_message(discord.Object(id='547731369988587530'), x.name)
 
 		
 players = {}
