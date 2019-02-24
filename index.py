2539380d592e0191af2dbf642d03c37a0a4111b3
TheLichIsBack <liche.tuto@gmail.com>
2019-02-24 16:16:27 +0100

    Update index.py

--- index.py
+++ index.py
@@ -481,6 +481,8 @@ async def on_message(message):
 				list_user_roles = []
 				for x in user.roles:
 					list_user_roles.append(x.name)
+				list_user_roles = str(list_user_roles)
+				print(list_user_roles)
 				info_mention.add_field(name="Avec les roles:", value=list_user_roles, inline=False)
 
 				await client.send_message(message.channel, embed=info_mention)
