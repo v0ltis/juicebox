676f4a452f3bfa2b3a7d25a0e47325103668cb57
v0ltis <40314287+v0ltis@users.noreply.github.com>
2019-02-27 16:08:35 +0100

    Update index.py

--- index.py
+++ index.py
@@ -34,7 +34,7 @@ memeaudio = ["https://www.youtube.com/watch?v=ma7TL8jJT0A",
 	     "https://www.youtube.com/watch?v=wPqo_q-HgmE"]
 
 
-news_emb = "Mise a jour 1.8 : \n Ajout du /memeaudio : Joue un meme dans votre salon vocal! \n Ajout d'un /info : Apprenez tout sur les memebres de votre serveur ... ou vous meme ! \n Nouvautée: Juicebox affiche désormais le nombre de serveurs , sur lequel il est connécté ! c'est pas super ?"    
+news_emb = "Mise a jour 1.8 : \n Ajout du ***__/memeaudio__*** : Joue un meme dans votre salon vocal! \n Ajout d'un ***__/info__*** : Apprenez tout sur les membres de votre serveur ... ou vous même! \n Nouvautée: Juicebox affiche désormais le nombre de serveurs sur lequel il est connecté ! c'est pas super ?"    
 
 nb_of_serv_where_i_am_connected = 0
 
