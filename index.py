5f623ce60200492293ad60c646d51673a198b50d
v0ltis <40314287+v0ltis@users.noreply.github.com>
2019-03-09 13:35:29 +0100

    Update index.py

--- index.py
+++ index.py
@@ -566,7 +566,7 @@ async def on_message(message):
 
 	for word in contents:
 		if word.upper() in merde:
-			await client.add_reaction(message.channel, emoji='💩')
+			await client.add_reaction(message.channel,emoji='💩')
 							
 						
 	if message.content.upper().startswith("YO"):
