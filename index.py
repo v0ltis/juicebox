b86ff5ce2521cc5d229ae071e7397c4ae50e401d
v0ltis <40314287+v0ltis@users.noreply.github.com>
2019-03-17 18:09:52 +0100

    Update index.py

--- index.py
+++ index.py
@@ -78,19 +78,6 @@ async def on_ready():
 
 	nb_of_serv_where_i_am_connected = 0
 	
-	for x in client.servers:
-		nb_of_serv_where_i_am_connected += 1
-		serv_co = str(nb_of_serv_where_i_am_connected) + ' serveurs'
-
-	await client.send_message(discord.Object(id='543490625773895681'), 'Redemarage effectué !')
-	while True:
-		await client.change_presence(game=discord.Game(name=serv_co))
-		await asyncio.sleep(15)
-		await client.change_presence(game=discord.Game(name="/help"))
-		await asyncio.sleep(15)
-		await client.change_presence(game=discord.Game(name="juicebot.github.io"))
-		await asyncio.sleep(15)
-
 	#upload l'emoji juicebot sur tout les serveurs
 	juiceboxemoji = False
 	for x in client.servers:
@@ -102,6 +89,24 @@ async def on_ready():
 			await client.create_custom_emoji(x,name='JuiceBox',image=open('JuiceBot.png','rb').read())
 		else:
 			juiceboxemoji = False
+	
+	for x in client.servers:
+		nb_of_serv_where_i_am_connected += 1
+		serv_co = str(nb_of_serv_where_i_am_connected) + ' serveurs'
+
+	await client.send_message(discord.Object(id='543490625773895681'), 'Redemarage effectué !')
+	while True:
+		nb_of_serv_where_i_am_connected = 0
+		for x in client.servers:
+			nb_of_serv_where_i_am_connected += 1
+			serv_co = str(nb_of_serv_where_i_am_connected) + ' serveurs'
+
+		await client.change_presence(game=discord.Game(name=serv_co))
+		await asyncio.sleep(15)
+		await client.change_presence(game=discord.Game(name="/help"))
+		await asyncio.sleep(15)
+		await client.change_presence(game=discord.Game(name="juicebot.github.io"))
+		await asyncio.sleep(15)
 			
 players = {}
 queues = {}
