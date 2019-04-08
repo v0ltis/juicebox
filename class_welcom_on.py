eec16333e9f7eed144e38c830ca15f8840ddfe62
v0ltis <40314287+v0ltis@users.noreply.github.com>
2019-04-08 12:08:30 +0200

    Update class_welcom_on.py

--- class_welcom_on.py
+++ class_welcom_on.py
@@ -2,5 +2,12 @@ import discord
 import asyncio
 from PIL import Image
 
-async def
-  welcim = Image.open("")
+class welcom():
+  def __init__(self,welcuserim):
+    self.client = client
+    
+ 
+    async def welcuserim(self):
+      welcim = Image.open("WELCtest.png")
+      image.rotate(90).save("welctest_1")
+      await client.send_file(message.channel, "welctest_1")
