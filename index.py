2787839b43fff99eddd890c97cfcf825fadb266a
v0ltis <40314287+v0ltis@users.noreply.github.com>
2019-04-12 09:50:14 +0200

    Update index.py

--- index.py
+++ index.py
@@ -88,7 +88,7 @@ async def on_message(message):
 		
 		if not auth.id.upper() in Const.bypass_list or channel.permissions_for(auth).administrator==False or channel.permissions_for(auth).manage_messages==False or channel.permissions_for(auth).manage_channels==False: 
 			await client.delete_message(message)
-			await client.send_message(message.channel, '<@{}>'.format(auth.id))
+		await client.send_message(message.channel, '<@{}>'.format(auth.id))
 			print(auth)
 
 		msg = ''
@@ -96,7 +96,7 @@ async def on_message(message):
 			msg = msg + x + ' '
 		msg = msg + message.content.split()[len(message.content.split())-1]
 
-		await client.send_message(channel,"```{}```".format(msg))
+		await client.send_message(channel,"{}".format(msg))
 		
 	elif message.content.upper().startswith(Const.prefix + "8BALL"):
 		await client.send_typing(message.channel)
