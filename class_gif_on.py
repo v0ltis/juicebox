c862eb36e68465b910f48dc8b50d689c75b37ef5
v0ltis <40314287+v0ltis@users.noreply.github.com>
2019-04-12 11:37:00 +0200

    Update class_gif_on.py

--- class_gif_on.py
+++ class_gif_on.py
@@ -15,7 +15,7 @@ async def gifsearch(message):
 	mess = "".join(arg[5:])
 	lien = str(linkstart) + str(mess) + str(linkmiddle) + str(GIPHY_KEY) + str(linkend)
 	
-	async with session.get(lien) as data:
+	 async with aiohttp.ClientSession() as data:
 		gif = json.dumps(data, sort_keys=True, indent=4)
 
 	await client.send_message(message.channel, gif)
