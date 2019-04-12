92586b18895090a61d90488a41ac6f09e78d4d28
v0ltis <40314287+v0ltis@users.noreply.github.com>
2019-04-12 11:51:47 +0200

    Update class_gif_on.py

--- class_gif_on.py
+++ class_gif_on.py
@@ -5,9 +5,11 @@ from constant_class import Juice_constants
 import json
 import aiohttp
 
+client.
+
 GIPHY_KEY = os.environ["TOKEN_GIPHY"]
 
-async def gifsearch(message):
+async def gifsearch(client,message):
 	linkstart = "http://api.giphy.com/v1/gifs/search?q="
 	linkmiddle = "&api_key="
 	linkend = "&limit=1"
@@ -16,6 +18,5 @@ async def gifsearch(message):
 	lien = str(linkstart) + str(mess) + str(linkmiddle) + str(GIPHY_KEY) + str(linkend)
 	async with aiohttp.ClientSession() as data:
 		gif = json.dumps(lien, sort_keys=True, indent=4)
-
 	await client.send_message(message.channel, gif)
 	
