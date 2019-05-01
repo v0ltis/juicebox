ea58116e74354e3ec39a99fecdc9b8fc8e418bdf
v0ltis <40314287+v0ltis@users.noreply.github.com>
2019-05-01 15:11:58 +0200

    Update index.py

--- index.py
+++ index.py
@@ -87,7 +87,7 @@ async def on_message(message):
 	elif message.content.upper().startswith("/SAY"):
 		args = message.content.split(" ")
 		auth = message.author
-		channel = message.server
+		channel = message.channel
 		
 		if not auth.id.upper() in Const.bypass_list or channel.permissions_for(auth).administrator==False or channel.permissions_for(auth).manage_messages==False or channel.permissions_for(auth).manage_channels==False: 
 			await client.delete_message(message)
